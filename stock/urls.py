e3bd96850d70ccee77481be0ceb4a7a9a7a4af00
Projekt 1 <denis.hosek@educanet.cz>
2023-11-30 10:18:37 +0000

    boots trap index

--- stock/urls.py
+++ stock/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("", include("stock.stocks.urls")),
+
     path("admin/", admin.site.urls),
 ]
 
