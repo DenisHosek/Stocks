050a9b91deddd51c8ebd61150f0ea21a7983fae0
Projekt 1 <denis.hosek@educanet.cz>
2024-01-25 10:12:24 +0000

    Ticker

--- stock/stocks/models.py
+++ stock/stocks/models.py
@@ -10,5 +10,11 @@ class Stock(models.Model):
     def __str__(self):
         return self.name_stock
 
+    def clean_ticker(self):
+        data = self.cleaned_data['ticker']
+        ticker = data.lower()
+        return ticker
+
+
     class Meta:
         unique_together = [["ticker", "user"]]
